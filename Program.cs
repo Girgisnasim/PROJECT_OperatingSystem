2828c36f289cda6053d2075f753e75cca5c669e9
Girgisnasim <91782130+Girgisnasim@users.noreply.github.com>
2022-05-21 18:55:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.IO;
-using System.Threading;
-using System.Text;
+using System;
+
+using System.Reflection;//reflection Assembly.GetExecutingAssembly().Location
+
+using System.IO;//directory
+
 using System.Collections.Generic;
 
+using System.Linq;
 
-namespace cmd
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace osProj
+
 {
-
     static class virtual_disk
     {
         public static FileStream disk;
@@ -131,56 +140,55 @@ namespace cmd
     }
     public class directory_entery
     {
-        public static char[] dir_name = new char[11];
-        public static byte dir_atlr;
-        public static byte[] dir_empty = new byte[12];
-        public static int fristcluster;
-        public static int dir_filesize;
+        char[] dir_name = new char[11];
+        byte dir_atlr;
+        byte[] dir_empty = new byte[12];
+        public int fristcluster;
+        int dir_filesize;
 
 
-        /* public directory_entery(char[] dir_name, byte dir_atlr, int dir_filesize)
-         {
-             dir_name = dir_name;
-             dir_atlr = dir_atlr;
-             dir_filesize = dir_filesize;
+        public directory_entery(char[] dir_name, byte dir_atlr, int dir_filesize)
+        {
+            this.dir_name = dir_name;
+            this.dir_atlr = dir_atlr;
+            this.dir_filesize = dir_filesize;
 
-         }*/
+        }
+        /*  public int firstcluster()
+          {
+              return fristcluster;
+          }*/
+
+
 
     }
     public class directory : directory_entery
     {
-        public static List<directory_entery> dirsfile = new List<directory_entery>();
+        public static List<directory_entery> dirsfile;
 
         directory parant;
 
-        public directory(char[] a, byte c, int frist_cluster, directory parent)
+        public directory(char[] dir_name, byte dir_atlr, int dir_filesize, directory parent) : base(dir_name, dir_atlr, dir_filesize)
         {
-            dir_name = a;
-            dir_atlr = c;
-            fristcluster = fristcluster;
-            this.parant = parant;
+            if (parent != null)
+                this.parant = parent;
 
-
-
-
-
-
-
-
-
-
+            dirsfile = new List<directory_entery>();
 
         }
 
 
 
-        static void writedirectory()
+        public void writedirectory()
         {
             byte[] b = new byte[dirsfile.Count * 32];
             int x = -1;
+
+
+
             for (int i = 0; i < dirsfile.Count; i++)
             {
-                byte[] c = BitConverter.GetBytes(dir_filesize);
+                byte[] c = dirsfile.OfType<byte>().ToArray();
                 Array.Reverse(c);
 
                 for (int j = 0; j < c.Length; j++)
@@ -205,7 +213,7 @@ namespace cmd
                 }
                 int laster_cluster = -1;
                 int cluster_index;
-                if (fristcluster == 0)
+                if (this.fristcluster == 0)
                 {
                     cluster_index = Mini_fat.get_available_block();
                     cluster_index = fristcluster;
@@ -218,17 +226,23 @@ namespace cmd
                 }
                 for (int i = 0; i < arry.Count; i++)
                 {
-
-                    virtual_disk.Write_cluster(cluster_index, arry[i]);
-                    Mini_fat.setclusterstatues(cluster_index, -1);
-                    if (cluster_index != -1)
-                        Mini_fat.setclusterstatues(laster_cluster, cluster_index);
-                    laster_cluster = cluster_index;
-                    cluster_index = Mini_fat.get_available_block();
+                    if (cluster_index != 0)
+                    {
+                        virtual_disk.Write_cluster(cluster_index, arry[i]);
+                        Mini_fat.setclusterstatues(cluster_index, -1);
+                        if (cluster_index != -1)
+                            Mini_fat.setclusterstatues(laster_cluster, cluster_index);
+                        laster_cluster = cluster_index;
+                        cluster_index = Mini_fat.get_available_block();
+                    }
 
 
                 }
-                Mini_fat.write_fat();
+                //   Mini_fat.write_fat();
+                if (this.parant != null)
+                {
+                    this.parant.writedirectory();
+                }
 
 
 
@@ -236,28 +250,37 @@ namespace cmd
 
 
             }
-
+            Mini_fat.write_fat();
 
         }
         public void read_Directory()
         {
-            if (fristcluster != 0)
+            if (this.fristcluster != 0)
             {
                 dirsfile = new List<directory_entery>();
-                int clusterindex = fristcluster;
-                int next = Mini_fat.setcluster(clusterindex);
+                int cluster = this.fristcluster;
+                int next = Mini_fat.setcluster(cluster);
+                List<byte> Is = new List<byte>();
                 do
                 {
-                    byte[] c = virtual_disk.read_cluster(clusterindex);
-                    for (int i = 0; i < c.Length; i++)
+                    Is.AddRange(virtual_disk.read_cluster(cluster));
+                    cluster = next;
+                    if (cluster != 0)
+                        next = Mini_fat.setcluster(cluster);
+
+
+
+                } while (next != 0);
+                for (int i = 0; i < Is.Count; i++)
+                {
+                    byte[] w = new byte[32];
+                    for (int j = i * 32, y = 0; y < w.Length && j < Is.Count; j++, y++)
                     {
-                        byte[] w = new byte[32];
-                        c.CopyTo(w, i * 32);
+
+                        w[y] = Is[j];
 
                     }
-
-
-                } while (clusterindex != 0);
+                }
 
 
             }
@@ -267,12 +290,83 @@ namespace cmd
 
 
     }
-    class Program
+    public class command
     {
+        public static void check(string x)
+        {
+            string[] q = x.Split(' ');
+            // Console.WriteLine(q.Length);
+            if (x == "cls"|| x == "CLS")
+                Clear();
+            else if (x == "quit"|| x == "QUIT")
+                Quit();
+            else if (x == "help"|| x == "HELP")
+                Help();
+            else if (q[0] == "md" || q[0] == "MD")
+                MD(q[1]);
+            else if (q[0] == "rd" || q[0] == "RD")
+                rd(q[1]);
+            else if (q[0] == "type"||q[0] == "TYPE")
+            {
+                for(int i=1; i< q.Length; i++)
+                {
+                    type(q[i]);
+                }
+            }
+
+            else if (q[0] == "del"|| q[0] == "DEL")
+            {
+                for(int i=1;i<q.Length;i++)
+                delete(q[i]);
+
+            }
+                
+            else if (q.Length >2 && q[0] == "copy")
+                copy(q[1],q[2]);
+
+            else if (q[0] == "help" && q.Length > 1)
+            {
+
+                if (q[1] == "clear")
+                    cls();
+                else if (q[1] == "quit")
+                    quit();
+                else if (q[1] == "help")
+                    help();
+                else if (q[1] == "md")
+                    made();
+                else if (q[1] == "rename")
+                    rename();
+                else if (q[1] == "type")
+                    Type();
+                else if (q[1] == "rd")
+                    remove();
+                else if (q[1] == "del")
+                    Delete();
+                else if (q[1] == "copy")
+                    COPY();
+                else
+                    Console.WriteLine(x + " is not recognized as an internal or external command program or batch file.");
+
+            }
+            else if (q.Length > 2 && q[0] == "rename")
+                Rename(q[1], q[2]);
+
+
+            else
+                Console.WriteLine(x + " is not recognized as an internal or external command program or batch file.");
+
+
+            //Console.WriteLine(x + " is not recognized as an internal or external command program or batch file.");
+
+
+        }
         static void Clear()
         {
             Console.Clear();
         }
+
+
         static void Quit()
         {
 
@@ -282,56 +376,187 @@ namespace cmd
 
             // System.Threading.Thread.Sleep(5000);
         }
+        static void MD(string a)
+        {
+            
+            string dir = $@"D:\{a}";
+            // If directory does not exist, create it
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+           
+            
+        }
+          
+
+        public static void rd(string a)
+        {
+            
+            string dir = $@"D:\{a}";
+            // If directory does not exist, create it
+            if (Directory.Exists(dir))
+            {
+                Directory.Delete(dir);
+            }
+            else
+                Console.WriteLine("the file is not exists");
+        }
+        public static  void Rename(string a ,string c)
+        {
+            rd(a);
+            MD(c);
+            Console.WriteLine("file is renamed");
+        }
+       public static void type(string a)
+        {
+            string[] lines = File.ReadAllLines($@"D:\{a}");
+
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+        public static void copy(string sour , string dest)
+        {
+            string sourceFile = $@"D:\{sour}";
+            string destinationFile = $@"E:\{dest}";
+           
+            System.IO.File.Copy(sourceFile, destinationFile);
+            Console.WriteLine("the process is done");
+            
+        }
+        public static void delete(string sour)
+        {
+            string sourceFile = $@"D:\{sour}";
+            
+
+            System.IO.File.Delete(sourceFile);
+
+        }
         static void Help()
         {
 
-            Console.WriteLine("CD             Displays the name of or changes the current directory.");
+            Console.WriteLine("  CD :       Displays the name of or changes the current directory.");
+            Console.WriteLine("  CLS :      Clears the screen.");
+            Console.WriteLine("  DIR  :     Displays a list of files and subdirectories in a directory.");
+            Console.WriteLine("  EXIT:      Quits the CMD.EXE program (command interpreter).");
+            Console.WriteLine("  COPY :     Copies one or more files to another location.");
+            Console.WriteLine("  DEL  :     Deletes one or more files.");
+            Console.WriteLine("  HELP:      Provides Help information for Windows commands.");
+            Console.WriteLine("  MD  :      Creates a directory.");
+            Console.WriteLine("  RD  :      Removes a directory.");
+            Console.WriteLine("  RENAME:    Renames a file or files.");
+            Console.WriteLine("  TYPE  :    Displays the contents of a text file.");
+
+        }
+        public static void cls()
+        {
+            Console.WriteLine();
             Console.WriteLine("CLS            Clears the screen.");
-            Console.WriteLine("DIR            Displays a list of files and subdirectories in a directory.");
+            Console.WriteLine();
+           
+
+        }
+        public static void quit()
+        {
+            Console.WriteLine();
             Console.WriteLine("EXIT           Quits the CMD.EXE program (command interpreter).");
-            Console.WriteLine("COPY           Copies one or more files to another location.");
-            Console.WriteLine("DEL            Deletes one or more files.");
+            Console.WriteLine();
+           
+        }
+        public static void help()
+        {
+            Console.WriteLine();
             Console.WriteLine("HELP           Provides Help information for Windows commands.");
+            Console.WriteLine();
+            
+        }
+        public static void made()
+        {
+            Console.WriteLine();
             Console.WriteLine("MD             Creates a directory.");
+            Console.WriteLine();
+            
+        }
+        public static void remove()
+        {
+            Console.WriteLine();
             Console.WriteLine("RD             Removes a directory.");
+            Console.WriteLine();
+            
+        }
+        public static void rename()
+        {
+            Console.WriteLine();
             Console.WriteLine("RENAME         Renames a file or files.");
+            Console.WriteLine();
+            
+        }
+        public static void Type()
+        {
+            Console.WriteLine();
             Console.WriteLine("TYPE           Displays the contents of a text file.");
+            Console.WriteLine();
+            
+        }
+        public static void COPY()
+        {
+            Console.WriteLine();
+            Console.WriteLine("COPY           Copies one or more files to another location.");
+            Console.WriteLine();
+
+        }
+        public static void Delete()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DEL            Deletes one or more files.");
+            Console.WriteLine();
 
         }
 
-        static void Main(string[] args)
+    }
+    public static class ExtendedMethod
+    {
+        public static void Rename(this FileInfo fileInfo, string newName)
         {
+            fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
+        }
+    }
+    class Program
 
-            string x, y;
+    {
 
+
+
+
+
+
+            static void Main(string[] args)
+
+            {
+
+
+                string x, y;
             virtual_disk.inti_disk(@"F:\cmd.txt");
 
+
             var Getdir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            string path = @"F:\ project";
-            string path2 = @"F:\cmd.txt";
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
+               
 
-            while (true)
-            {
-                Console.Write(Getdir + ">> ");
-                y = Console.ReadLine();
-                if (y == "cls")
-                    Clear();
-                else if (y == "quit")
-                    Quit();
-                else if (y == "help")
-                    Help();
-                else
-                    Console.WriteLine(y + " is not recognized as an internal or external command program or batch file.");
+                while (true)
+                {
+                    Console.Write(Getdir + ">> ");
+                    y = Console.ReadLine();
+               
+                command.check(y);
 
+                       
+
+
+                }
 
             }
 
+        
 
-
-        }
     }
 }
